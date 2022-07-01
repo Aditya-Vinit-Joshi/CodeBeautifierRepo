fa35df8f83b06f609a4c43b978e903f39b2850e7
Aditya Joshi <adityavjoshi@outlook.com>
2022-07-01 11:50:26 +0400

    Initial commit

---
+++ PracticeWebAPI/Controllers/CodeDecorationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PracticeWebAPI.Repository;
+
+namespace PracticeWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CodeDecorationController : ControllerBase
+    {
+        private readonly IBeautifyCodeRepository _beautify;
+
+        public CodeDecorationController(IBeautifyCodeRepository beautify)
+        {
+            _beautify = beautify;       
+        }
+
+        [HttpPost("{id}/{code}")]
+        public IActionResult FormatCode([FromRoute] string code,int id)
+        {
+            var modifiedCode = _beautify.Beautify(code, id);
+            return Ok(modifiedCode);
+        }
+    }
+}
